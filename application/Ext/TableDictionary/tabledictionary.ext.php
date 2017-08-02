868759f79e9bbb8ce331e72ea78cf3120608e241
Rehman Ahmad <rehman.ahmad@RT-LT-268.rolustech.net>
2017-08-02 12:17:36 +0500

    Auguest2

---
+++ application/Ext/TableDictionary/tabledictionary.ext.php
@@ -0,0 +1,99 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/rt_module_manager_rt_module_student.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/rt_module_manager_rt_module_studentMetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/rt_module_instructor_rt_module_student.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/rt_module_instructor_rt_module_studentMetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/rt_module_student_accounts.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/rt_module_student_accountsMetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/rt_module_manager_rt_module_instructor.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/rt_module_manager_rt_module_instructorMetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/rt2_m1_rt2_m2_1.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/rt2_m1_rt2_m2_1MetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/rt2_m2_rt2_m1_2.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/rt2_m2_rt2_m1_2MetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/three_aye_three_bee_1.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/three_aye_three_bee_1MetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/three_bee_three_aye_1.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/three_bee_three_aye_1MetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/three_aye_three_bee.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/three_aye_three_beeMetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/three_bee_three_aye.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/three_bee_three_ayeMetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/three_cee_three_aye.php
+
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/three_cee_three_ayeMetaData.php');
+
+
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/three_cee_three_aye_1.php
+
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/three_cee_three_aye_1MetaData.php');
+
+
+?>
